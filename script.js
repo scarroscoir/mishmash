689f59ed307d47ebc14edd5f6e4b0d7acdb48cbd
scarroscoir <seananolivermanfredkerr@gmail.com>
2024-10-25 18:25:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-// Check Password Function
-function checkPassword() {
-    const password = document.getElementById('password').value;
-    const correctPassword = 'NachBhfuilSeanánGoHíontachtArFad'; // Replace with your password
-
-    if (password === correctPassword) {
-        document.getElementById('password-modal').style.display = 'none';
-        openCurtain();
-    } else {
-        alert('Incorrect Password');
-    }
-}
-
-// Curtain Animation Function
-function openCurtain() {
-    const leftCurtain = document.getElementById('curtain-left');
-    const rightCurtain = document.getElementById('curtain-right');
-
-    leftCurtain.style.transform = 'translateX(-100%)';
-    rightCurtain.style.transform = 'translateX(100%)';
-
-    setTimeout(() => {
-        document.getElementById('portfolio-content').style.display = 'block';
-        document.body.style.overflow = 'auto'; // Allow scrolling after curtains open
-    }, 2000); // Delay to match curtain animation duration
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://github.com/SOMK17/somk17.github.io/blob/main/script.js</string>
+</dict>
+</plist>
